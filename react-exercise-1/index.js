4579ecaad6f36a588f0be1074ad03716782eb86b
Nihat Çetin <96945187+nihatctnn@users.noreply.github.com>
2023-08-24 00:06:46 +0300

    Delete react-exercise-1 directory

--- react-exercise-1/index.js
+++
@@ -1,8 +0,0 @@
-import getData from "./app.js";
-
-// Importing the getData function from the app.js file.
-
-// Calling the getData function with an argument of 1.
-// This will initiate the process of fetching data for the user with ID 1.
-
-getData(1);
